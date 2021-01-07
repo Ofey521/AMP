2d2fbf3812485d729b94fac1d439ed51091287b4
ofey <mat.janczura@gmail.com>
2021-01-07 22:59:36 +0100

    praca nad sliderem

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,93 +1,92 @@
-$(function() {
-    let pdfDoc = null,
-        pageNum = 1,
-        pageRendering = false,
-        pageNumPending = null;
-  
-    const scale = 5.0,
-          canvas = document.getElementById('pdf-canvas'),
-          pnum = document.getElementById('page-num')
-          ctx = canvas.getContext('2d');
-  
-    /**
-     * Get page info from document, resize canvas accordingly, and render page.
-     * @param num Page number.
-     */
-    function renderPage(num) {
-      pageRendering = true;
-      
-      // Using promise to fetch the page
-      pdfDoc.getPage(num).then(function(page) {
-        const page_viewport = page.getViewport(scale);
-        canvas.height = page_viewport.height;
-        canvas.width = page_viewport.width;
-  
-        // Render PDF page into canvas context
-        const renderContext = {
-          canvasContext: ctx,
-          viewport: page_viewport
-        };
-        const renderTask = page.render(renderContext);
-  
-        // Wait for rendering to finish
-        renderTask.promise.then(function() {
-          pageRendering = false;
-          if (pageNumPending !== null) {
-            // New page rendering is pending
-            renderPage(pageNumPending);
-            pageNumPending = null;
-          }
-        });
-      });
-  
-      // Update page counters
-      $(pnum).text(num);
-    }
-  
-    /**
-     * If another page rendering in progress, waits until the rendering is
-     * finised. Otherwise, executes rendering immediately.
-     */
-    function queueRenderPage(num) {
-      if (pageRendering) {
-        pageNumPending = num;
-      } else {
-        renderPage(num);
-      }
-    }
-  
-    /**
-     * Displays previous page.
-     */
-    $(".carousel-control-prev").click(function() {
-      if (pageNum > 1) {
-        pageNum--;
-        queueRenderPage(pageNum);
+const url = '/static/pdf.pdf';
+
+
+let pdfDoc = null,
+  pageNum = 1,
+  pageIsRendering = false,
+  pageNumIsPending = null;
+
+const scale = 1.5,
+  canvas = document.querySelector('#pdf-canvas'),
+  ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+  pageIsRendering = true;
+
+  // Get page
+  pdfDoc.getPage(num).then(page => {
+    // Set scale
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport
+    };
+
+    page.render(renderCtx).promise.then(() => {
+      pageIsRendering = false;
+
+      if (pageNumIsPending !== null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
       }
     });
-  
-    /**
-     * Displays next page.
-     */
-    $(".carousel-control-next").click(function() {
-      if (pageNum < pdfDoc.numPages) {
-        pageNum++;
-        queueRenderPage(pageNum);
-      }
-    });
-  
-    /**
-     * Asynchronously downloads PDF.
-     */
-    (function() {
-      const url = $(canvas).data("/static/PROJEKT-STORNY.pdf");
-      pdfjsLib.getDocument(url).then(function(pdfDoc_) {
-        pdfDoc = pdfDoc_;
-        $("#page-count").text(pdfDoc.numPages);
-  
-        // Initial/first page rendering
-        renderPage(pageNum);
-      });
-    })();
+
+    // Output current page
+    document.querySelector('#page-num').textContent = num;
   });
-  
+};
+
+// Check for pages rendering
+const queueRenderPage = num => {
+  if (pageIsRendering) {
+    pageNumIsPending = num;
+  } else {
+    renderPage(num);
+  }
+};
+
+// Show Prev Page
+const showPrevPage = () => {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  queueRenderPage(pageNum);
+};
+
+// Show Next Page
+const showNextPage = () => {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+};
+
+// Get Document
+pdfjsLib
+  .getDocument(url)
+  .promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_;
+
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+    renderPage(pageNum);
+  })
+  .catch(err => {
+    // Display error
+    const div = document.createElement('div');
+    div.className = 'error';
+    div.appendChild(document.createTextNode(err.message));
+    document.querySelector('body').insertBefore(div, canvas);
+    // Remove top bar
+    document.querySelector('.top-bar').style.display = 'none';
+  });
+
+// Button Events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
