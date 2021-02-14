6f3e90fa8101d1603d2f2eb16820b6ff6a5f37d1
ofey <mat.janczura@gmail.com>
2021-02-14 13:00:15 +0100

    Poprawa buttona, elementów graficznych

--- static/js/swith.js
+++ static/js/swith.js
@@ -1,10 +1,13 @@
    function switch_text() {
     var x = document.getElementById("myDIV");
-    if (x.innerHTML[0] === "D")
-     {
+    var y = document.getElementById("option 1")
+    if (x.innerHTML[5] === "o")
+     {  y.value = "O nieregularnym środowisku";
+        y.innerHTML = "Antologia"
         x.innerHTML = "<p>Przyczyną powstania niniejszej antologii są prowadzone przeze mnie badania nad lokalnym środowiskiem poetyckim w latach 2018 – 2020 oraz próba uchwycenia tożsamości młodych autorów znajdujących się na granicy pola poetyckiego. Reguły gry są kolejną cegiełką w dyskusji o przynależności młodych autorów do środowiska poetyckiego na terenie Wrocławia z całym jego uposażeniem, tj. aktualnymi właściwościami i wartościami symbolicznymi obowiązującymi w polu, ale też wytrychem do szerszego spojrzenia na warunki przenikania debiutantów w tkankę środowiska. Antologia jest więc krokiem w stronę ponownego odczytania „specyficznego typu samowiedzy”, którą można określić mianem tożsamości.</p>";} 
     else {
-    x.innerHTML = "D<p>Projekt jest jednocześnie związany z badaniami nad rozwojem poezjotwórczości we Wrocławiu i jego funkcji względem regionu. Rozwojowi myśli nad kształtem wrocławskiegośrodowiska młodopoetyckiego towarzyszą pytania o jego charakterystykę na tle trendówogólnopolskich, ustalenie pozycji względem heteronomii oficjalnego środowiskapoetyckiego, instytucji miejskich czy kulturalnych oraz o warunki rozwoju lokalnych młodych twórców. Stawia pytanie o samoświadomość środowiskową młodych autorów, ich kontakty w przestrzeni lokalnej oraz o trend kształtowania się środowiska online, a więc internetowy charakter (typ) lokalności, eksterytorialności i kryterium przynależności. </p> <br><p> Zachęcam do przesyłania utworów poetyckich, które będą publikowane na stronie oraz posłużą do powstania współczesnej antologii tekstów młodopoetyckich, będących świadectwem głosu pokolenia lat 20 XXI w.</p><br><p>Projekt realizowany w ramach stypendium artystycznego prezydenta Wrocławia z zakresuupowszechniania kultury - Animacja młodego wrocławskiego środowiska poetyckiego</p>";
+    y.innerHTML = "O nieregularnym środowisku";
+    x.innerHTML = "<p>Projekt jest jednocześnie związany z badaniami nad rozwojem poezjotwórczości we Wrocławiu i jego funkcji względem regionu. Rozwojowi myśli nad kształtem wrocławskiegośrodowiska młodopoetyckiego towarzyszą pytania o jego charakterystykę na tle trendówogólnopolskich, ustalenie pozycji względem heteronomii oficjalnego środowiskapoetyckiego, instytucji miejskich czy kulturalnych oraz o warunki rozwoju lokalnych młodych twórców. Stawia pytanie o samoświadomość środowiskową młodych autorów, ich kontakty w przestrzeni lokalnej oraz o trend kształtowania się środowiska online, a więc internetowy charakter (typ) lokalności, eksterytorialności i kryterium przynależności. </p> <br><p> Zachęcam do przesyłania utworów poetyckich, które będą publikowane na stronie oraz posłużą do powstania współczesnej antologii tekstów młodopoetyckich, będących świadectwem głosu pokolenia lat 20 XXI w.</p><br><p>Projekt realizowany w ramach stypendium artystycznego prezydenta Wrocławia z zakresuupowszechniania kultury - Animacja młodego wrocławskiego środowiska poetyckiego</p>";
         }
     }
 
